496324c6ceb20e01e3c1ced550adf175c2844fd4
Peter Shkenev <petershh@disroot.org>
2022-09-02 09:24:00 +0300

    date822fmt.c: use skalibs

--- date822fmt.c
+++ date822fmt.c
@@ -1,3 +1,5 @@
+#include <skalibs/types.h>
+
 #include "date822fmt.h"
 
 #include "datetime.h"
@@ -14,17 +16,17 @@ struct datetime *dt;
   unsigned int i;
   unsigned int len;
   len = 0;
-  i = fmt_uint(s,dt->mday); len += i; if (s) s += i;
+  i = uint_fmt(s,dt->mday); len += i; if (s) s += i;
   i = fmt_str(s," "); len += i; if (s) s += i;
   i = fmt_str(s,montab[dt->mon]); len += i; if (s) s += i;
   i = fmt_str(s," "); len += i; if (s) s += i;
-  i = fmt_uint(s,dt->year + 1900); len += i; if (s) s += i;
+  i = uint_fmt(s,dt->year + 1900); len += i; if (s) s += i;
   i = fmt_str(s," "); len += i; if (s) s += i;
-  i = fmt_uint0(s,dt->hour,2); len += i; if (s) s += i;
+  i = uint0_fmt(s,dt->hour,2); len += i; if (s) s += i;
   i = fmt_str(s,":"); len += i; if (s) s += i;
-  i = fmt_uint0(s,dt->min,2); len += i; if (s) s += i;
+  i = uint0_fmt(s,dt->min,2); len += i; if (s) s += i;
   i = fmt_str(s,":"); len += i; if (s) s += i;
-  i = fmt_uint0(s,dt->sec,2); len += i; if (s) s += i;
+  i = uint0_fmt(s,dt->sec,2); len += i; if (s) s += i;
   i = fmt_str(s," -0000\n"); len += i; if (s) s += i;
   return len;
 }
