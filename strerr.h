c260ab08f6c946f84a8efd42300039d90fdc6c2e
Peter Shkenev <petershh@disroot.org>
2023-03-30 16:58:53 +0300

    strerr.h: make sure it does not conflict with skalibs

--- strerr.h
+++ strerr.h
@@ -6,9 +6,9 @@
 struct strerr
  {
   struct strerr *who;
-  char *x;
-  char *y;
-  char *z;
+  char const *x;
+  char const *y;
+  char const *z;
  }
 ;
 
@@ -16,8 +16,8 @@ extern struct strerr strerr_sys;
 extern void strerr_sysinit();
 
 extern char *strerr();
-extern void strerr_warn();
-extern void _noreturn_ strerr_die();
+extern void qstrerr_warn();
+extern void _noreturn_ qstrerr_die();
 
 #define STRERR(r,se,a) \
 { se.who = 0; se.x = a; se.y = 0; se.z = 0; return r; }
@@ -28,55 +28,55 @@ extern void _noreturn_ strerr_die();
 { se.who = &strerr_sys; se.x = a; se.y = b; se.z = c; return r; }
 
 #define strerr_warn6(x1,x2,x3,x4,x5,x6,se) \
-strerr_warn((x1),(x2),(x3),(x4),(x5),(x6),(struct strerr *) (se))
+qstrerr_warn((x1),(x2),(x3),(x4),(x5),(x6),(struct strerr *) (se))
 #define strerr_warn5(x1,x2,x3,x4,x5,se) \
-strerr_warn((x1),(x2),(x3),(x4),(x5),NULL,(struct strerr *) (se))
+qstrerr_warn((x1),(x2),(x3),(x4),(x5),NULL,(struct strerr *) (se))
 #define strerr_warn4(x1,x2,x3,x4,se) \
-strerr_warn((x1),(x2),(x3),(x4),NULL,NULL,(struct strerr *) (se))
+qstrerr_warn((x1),(x2),(x3),(x4),NULL,NULL,(struct strerr *) (se))
 #define strerr_warn3(x1,x2,x3,se) \
-strerr_warn((x1),(x2),(x3),NULL,NULL,NULL,(struct strerr *) (se))
+qstrerr_warn((x1),(x2),(x3),NULL,NULL,NULL,(struct strerr *) (se))
 #define strerr_warn2(x1,x2,se) \
-strerr_warn((x1),(x2),NULL,NULL,NULL,NULL,(struct strerr *) (se))
+qstrerr_warn((x1),(x2),NULL,NULL,NULL,NULL,(struct strerr *) (se))
 #define strerr_warn1(x1,se) \
-strerr_warn((x1),NULL,NULL,NULL,NULL,NULL,(struct strerr *) (se))
+qstrerr_warn((x1),NULL,NULL,NULL,NULL,NULL,(struct strerr *) (se))
 
 #define strerr_die6(e,x1,x2,x3,x4,x5,x6,se) \
-strerr_die((e),(x1),(x2),(x3),(x4),(x5),(x6),(struct strerr *) (se))
+qstrerr_die((e),(x1),(x2),(x3),(x4),(x5),(x6),(struct strerr *) (se))
 #define strerr_die5(e,x1,x2,x3,x4,x5,se) \
-strerr_die((e),(x1),(x2),(x3),(x4),(x5),NULL,(struct strerr *) (se))
+qstrerr_die((e),(x1),(x2),(x3),(x4),(x5),NULL,(struct strerr *) (se))
 #define strerr_die4(e,x1,x2,x3,x4,se) \
-strerr_die((e),(x1),(x2),(x3),(x4),NULL,NULL,(struct strerr *) (se))
+qstrerr_die((e),(x1),(x2),(x3),(x4),NULL,NULL,(struct strerr *) (se))
 #define strerr_die3(e,x1,x2,x3,se) \
-strerr_die((e),(x1),(x2),(x3),NULL,NULL,NULL,(struct strerr *) (se))
+qstrerr_die((e),(x1),(x2),(x3),NULL,NULL,NULL,(struct strerr *) (se))
 #define strerr_die2(e,x1,x2,se) \
-strerr_die((e),(x1),(x2),NULL,NULL,NULL,NULL,(struct strerr *) (se))
+qstrerr_die((e),(x1),(x2),NULL,NULL,NULL,NULL,(struct strerr *) (se))
 #define strerr_die1(e,x1,se) \
-strerr_die((e),(x1),NULL,NULL,NULL,NULL,NULL,(struct strerr *) (se))
+qstrerr_die((e),(x1),NULL,NULL,NULL,NULL,NULL,(struct strerr *) (se))
 
 #define strerr_die6sys(e,x1,x2,x3,x4,x5,x6) \
-strerr_die((e),(x1),(x2),(x3),(x4),(x5),(x6),&strerr_sys)
+qstrerr_die((e),(x1),(x2),(x3),(x4),(x5),(x6),&strerr_sys)
 #define strerr_die5sys(e,x1,x2,x3,x4,x5) \
-strerr_die((e),(x1),(x2),(x3),(x4),(x5),NULL,&strerr_sys)
+qstrerr_die((e),(x1),(x2),(x3),(x4),(x5),NULL,&strerr_sys)
 #define strerr_die4sys(e,x1,x2,x3,x4) \
-strerr_die((e),(x1),(x2),(x3),(x4),NULL,NULL,&strerr_sys)
+qstrerr_die((e),(x1),(x2),(x3),(x4),NULL,NULL,&strerr_sys)
 #define strerr_die3sys(e,x1,x2,x3) \
-strerr_die((e),(x1),(x2),(x3),NULL,NULL,NULL,&strerr_sys)
+qstrerr_die((e),(x1),(x2),(x3),NULL,NULL,NULL,&strerr_sys)
 #define strerr_die2sys(e,x1,x2) \
-strerr_die((e),(x1),(x2),NULL,NULL,NULL,NULL,&strerr_sys)
+qstrerr_die((e),(x1),(x2),NULL,NULL,NULL,NULL,&strerr_sys)
 #define strerr_die1sys(e,x1) \
-strerr_die((e),(x1),NULL,NULL,NULL,NULL,NULL,&strerr_sys)
+qstrerr_die((e),(x1),NULL,NULL,NULL,NULL,NULL,&strerr_sys)
 
 #define strerr_die6x(e,x1,x2,x3,x4,x5,x6) \
-strerr_die((e),(x1),(x2),(x3),(x4),(x5),(x6),NULL)
+qstrerr_die((e),(x1),(x2),(x3),(x4),(x5),(x6),NULL)
 #define strerr_die5x(e,x1,x2,x3,x4,x5) \
-strerr_die((e),(x1),(x2),(x3),(x4),(x5),NULL,NULL)
+qstrerr_die((e),(x1),(x2),(x3),(x4),(x5),NULL,NULL)
 #define strerr_die4x(e,x1,x2,x3,x4) \
-strerr_die((e),(x1),(x2),(x3),(x4),NULL,NULL,NULL)
+qstrerr_die((e),(x1),(x2),(x3),(x4),NULL,NULL,NULL)
 #define strerr_die3x(e,x1,x2,x3) \
-strerr_die((e),(x1),(x2),(x3),NULL,NULL,NULL,NULL)
+qstrerr_die((e),(x1),(x2),(x3),NULL,NULL,NULL,NULL)
 #define strerr_die2x(e,x1,x2) \
-strerr_die((e),(x1),(x2),NULL,NULL,NULL,NULL,NULL)
+qstrerr_die((e),(x1),(x2),NULL,NULL,NULL,NULL,NULL)
 #define strerr_die1x(e,x1) \
-strerr_die((e),(x1),NULL,NULL,NULL,NULL,NULL,NULL)
+qstrerr_die((e),(x1),NULL,NULL,NULL,NULL,NULL,NULL)
 
 #endif
