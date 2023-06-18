69aba9f41bfd2f7cd9783cc0a6765ae57dfd71e9
Peter Shkenev <petershh@disroot.org>
2023-06-18 22:20:09 +0300

    fmtqfn.c: migrate to skalibs

--- fmtqfn.c
+++ fmtqfn.c
@@ -1,13 +1,11 @@
+#include <skalibs/types.h>
+
 #include "fmtqfn.h"
 
 #include "fmt.h"
 #include "auto_split.h"
 
-unsigned int fmtqfn(s,dirslash,id,flagsplit)
-char *s;
-char *dirslash;
-unsigned long id;
-int flagsplit;
+unsigned int fmtqfn(char *s, char *dirslash, unsigned long id, int flagsplit)
 {
  unsigned int len;
  unsigned int i;
@@ -16,10 +14,10 @@ int flagsplit;
  i = fmt_str(s,dirslash); len += i; if (s) s += i;
  if (flagsplit)
   {
-   i = fmt_ulong(s,id % auto_split); len += i; if (s) s += i;
+   i = ulong_fmt(s,id % auto_split); len += i; if (s) s += i;
    i = fmt_str(s,"/"); len += i; if (s) s += i;
   }
- i = fmt_ulong(s,id); len += i; if (s) s += i;
+ i = ulong_fmt(s,id); len += i; if (s) s += i;
  if (s) *s++ = 0; ++len;
  return len;
 }
