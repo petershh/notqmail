f4ebbf552aaf04c55fb61da2af07d52182307d6b
Peter Shkenev <petershh@disroot.org>
2023-06-25 23:38:29 +0300

    fmtqfn.h: introduce min size macro

--- fmtqfn.h
+++ fmtqfn.h
@@ -1,8 +1,13 @@
 #ifndef FMTQFN_H
 #define FMTQFN_H
 
+#include <skalibs/types.h>
+
 extern unsigned int fmtqfn();
 
 #define FMTQFN 40 /* maximum space needed, if len(dirslash) <= 10 */
+
+/* minimum space required */
+#define FMTQFN_MIN ULONG_FMT + 1 + ULONG_FMT + 1
 
 #endif
